a3e8e2911bb0d27d13ff7bc423395f16c8fc0fe3
Wesley Monteiro <wesley.monteiro@cadena.com.br>
2019-03-28 10:50:59 -0300

    updates index of videos

--- challenge/app/controllers/videos_controller.rb
+++ challenge/app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
   def index
+    @videos = (current_user.admin? ? Video.all : Video.where(user_id: current_user.id)).includes(:user)
   end
 
   def new
