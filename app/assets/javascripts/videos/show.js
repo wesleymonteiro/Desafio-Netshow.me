e56089aa649ca51f99f6825d441b0910fc243e0f
Unknown <weslito96@gmail.com>
2019-03-30 22:23:57 -0300

    fixes turbolink with videojs

--- app/assets/javascripts/videos/show.js
+++ app/assets/javascripts/videos/show.js
@@ -1,16 +1,26 @@
 $(document).ready(()=>{
   let $videoTag = $("#video-tag")
-  let options = {hls: {
-    overrideNative: true,
-    nativeAudioTracks: false,
-    nativeVideoTracks: false
-  }}
-  
-  let video = videojs("video-tag", {flash: options, html5: options})
-  video.ready(()=>{
-    $.ajax({
-      url: $videoTag.data("url"),
-      type: "POST"
+  if ($videoTag.length > 0) {
+    let options = {hls: {
+      overrideNative: true,
+      nativeAudioTracks: false,
+      nativeVideoTracks: false
+    }}
+    
+    let video = videojs("video-tag", {flash: options, html5: options})
+    video.on('play', function(){
+      $.ajax({
+        url: $videoTag.data("url"),
+        type: "POST"
+      })
     })
-  })
+  
+    $(document).on('turbolinks:load', ()=>{
+      video = videojs("video-tag")
+    })
+    $(document).on('turbolinks:before-visit', ()=>{
+      video = videojs("video-tag")
+      video.dispose()
+    })
+  }
 })
