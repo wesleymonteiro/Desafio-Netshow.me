b1c19e8c5af8a8543c0e2a75d62c4bd27c8c0924
Unknown <weslito96@gmail.com>
2019-03-30 22:33:48 -0300

    fixes not updating view

--- app/assets/javascripts/videos/show.js
+++ app/assets/javascripts/videos/show.js
@@ -1,22 +1,15 @@
 $(document).ready(()=>{
   let $videoTag = $("#video-tag")
   if ($videoTag.length > 0) {
-    let options = {hls: {
-      overrideNative: true,
-      nativeAudioTracks: false,
-      nativeVideoTracks: false
-    }}
-    
-    let video = videojs("video-tag", {flash: options, html5: options})
-    video.on('play', function(){
-      $.ajax({
-        url: $videoTag.data("url"),
-        type: "POST"
-      })
-    })
-  
     $(document).on('turbolinks:load', ()=>{
       video = videojs("video-tag")
+      video.on('play', ()=>{
+        if (video.currentTime() == 0)
+          $.ajax({
+            url: $videoTag.data("url"),
+            type: "POST"
+          })
+      })
     })
     $(document).on('turbolinks:before-visit', ()=>{
       video = videojs("video-tag")
